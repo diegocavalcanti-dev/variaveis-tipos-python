e677f1fc04cc5ac108245e27d59f911ba1cd2e09
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-11-02 14:03:13 -0300

    variaveis pyhton

---
+++ test.py
@@ -0,0 +1,52 @@
+
+# Definição
+idade = 30 
+print(idade)
+
+idade = 27
+print(idade)
+
+nome = "diego"
+print(nome)
+
+
+# Tipos nativos
+preco = 1000
+tipo_preco = type(preco)
+
+print(preco)
+print(tipo_preco)
+
+juros = 0.05
+tipo_juros = type(juros)
+
+print(juros)
+print(tipo_juros)
+
+
+# Tipos de texto: strings
+primeiro_nome = "Diego"
+
+print(primeiro_nome)
+print(type(primeiro_nome))
+
+pais = 'Brasil'
+
+print(pais)
+print(type(pais))
+
+
+
+# Tipos lógicos: booleanos (bool):
+usuario_maior_de_idade = True
+
+print(usuario_maior_de_idade)
+print(type(usuario_maior_de_idade))
+
+
+
+# Tipo vazio (NoneType):
+telefone_fixo = None
+
+print(telefone_fixo)
+print(type(telefone_fixo))
