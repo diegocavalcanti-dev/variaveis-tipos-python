35fe408e8122f9ae4e8db05fb261ada1cdec9f5e
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-11-03 12:55:30 -0300

    atualização

--- test.py
+++ test.py
@@ -50,3 +50,71 @@ telefone_fixo = None
 
 print(telefone_fixo)
 print(type(telefone_fixo))
+
+# ///////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////
+
+
+
+# Números
+
+
+### **3.2. Definição** 
+
+# Armazenam **valores numéricos**: 
+
+# *   `10, 37, 500` (inteiros);
+# *   `0.333, 10.1` (decimais);
+# *   `1 + 2j` (complexos).
+
+
+
+# São dos tipos:
+
+# *   `int` (inteiros);
+# *   `float` (decimais);
+# *   `complex` (complexos).
+
+
+
+print(type(37))
+print(type(10.1))
+print(type(1 + 2j))
+
+preco = 47
+quantidade = 0.250
+
+total_a_pagar = quantidade * preco
+print(total_a_pagar)
+
+#############################
+
+# Ticket médio diário do dia 19/01.
+#svv = soma do valor das vendas
+svv_19 = 153.98
+sqv_19 = 3
+
+tkt_19 = svv_19 / sqv_19
+print(tkt_19)
+
+# Ticket médio diário do dia 20/01.
+svv_20 = 337.01
+sqv_20 = 7
+
+tkt_20 = svv_20 / sqv_20
+print(tkt_20)
+
+# Ticket médio diário do dia 23/01.
+svv_23 = 295.33
+sqv_23 = 5
+
+tkt_23 = svv_23 / sqv_23
+print(tkt_23)
+
+
+# Ticket médio
+tkt = (tkt_19 + tkt_20 + tkt_23) / 3
+print(tkt)
+
